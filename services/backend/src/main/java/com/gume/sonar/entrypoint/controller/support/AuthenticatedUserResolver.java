351dd3def3b7640d35958f57097d6c624863bcfd
vitorgume <vitorhvvieira@gmail.com>
2026-06-26 10:36:54 -0300

    Adicionando log para entender problema de autenticação

--- services/backend/src/main/java/com/gume/sonar/entrypoint/controller/support/AuthenticatedUserResolver.java
+++ services/backend/src/main/java/com/gume/sonar/entrypoint/controller/support/AuthenticatedUserResolver.java
@@ -3,16 +3,20 @@ package com.gume.sonar.entrypoint.controller.support;
 import com.gume.sonar.application.usecase.UserUseCase;
 import com.gume.sonar.domain.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AuthenticatedUserResolver {
 
     private final UserUseCase userUseCase;
 
     public User resolve(Authentication authentication) {
+        log.info("Resolvendo usuário: {}, {}", authentication.getName(), authentication.getPrincipal().toString());
         return userUseCase.findByEmail(authentication.getPrincipal().toString());
     }
 }
